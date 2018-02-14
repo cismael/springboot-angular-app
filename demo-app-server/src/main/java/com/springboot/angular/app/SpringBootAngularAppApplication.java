bf4e8ce54e4c563b14d26f3fa585245b7330e36f
Ismaël COULIBALY <c.ismael@live.fr>
2018-02-15 00:54:28 +0100

    Adding backend using springboot

---
+++ demo-app-server/src/main/java/com/springboot/angular/app/SpringBootAngularAppApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.angular.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularAppApplication.class, args);
+	}
+}
