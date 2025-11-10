aa9c584807bec6df7373c49dcaa6ece309102d47
cismael <c.ismael@live.fr>
2025-11-10 21:40:21 +0100

    Upgrade frontend to angular 18

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const path = require('path');
 
 module.exports = function (config) {
   config.set({
@@ -9,14 +10,16 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+    coverageReporter: {
+      type: 'html',
+      dir: path.join(__dirname, './coverage'),
+      subdir: '.',
       fixWebpackSourcePaths: true
     },
     angularCli: {
